bfc0690922c64ad9de02ee6df35c252e3b8f1821
andywiecko <awiecko@us.edu.pl>
2019-11-09 12:18:32 +0100

    cols added

---
+++ Lekcja05/col2.py
@@ -0,0 +1,18 @@
+#!/bin/python
+########################################
+# przykładowe rozwiązanie kolokwium
+# plik zad2.py
+########################################
+# grupa A, zadanie 2
+# Andrzej Więckowski
+# Old Man Willow, 09-11-2019
+
+def Wyrazy(lista):
+    return " ".join(lista)
+
+def main():
+    wyrazy = Wyrazy(['ala','ma','kota'])
+    print("Zlepek:",wyrazy)
+
+if __name__=="__main__":
+    main()
